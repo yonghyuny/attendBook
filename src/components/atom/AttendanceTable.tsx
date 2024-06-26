967666fbcdea2248e0055eee77934db27e28db0e
yonghyuny <lovelove4695@naver.com>
2024-06-26 14:59:17 +0900

    table modify

--- src/components/atom/AttendanceTable.tsx
+++ src/components/atom/AttendanceTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 import axios from "axios";
 
 interface Attendance {
@@ -24,23 +24,46 @@ const AttendanceTable = () => {
     getData();
   }, []);
 
+  const tableStyle: CSSProperties = {
+    maxWidth: "1200px",
+    width: "100%",
+    margin: "0 auto",
+  };
+  const attendTableStyle: CSSProperties = {
+    maxWidth: "1200px",
+    width: "100%",
+    margin: "0 auto",
+    border: "1px solid black",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  };
+
+  const attendContentStyle: CSSProperties = {
+    width: "400px",
+    borderRight: "1px solid black",
+    textAlign: "center",
+  };
   return (
-    <table>
+    <table style={tableStyle}>
+      ///
       <thead>
-        <tr>
-          <th>날짜</th>
-          <th>반이름</th>
-          <th>학생이름</th>
-          <th>출결상태</th>
+        <tr style={attendTableStyle}>
+          <th style={attendContentStyle}>날짜</th>
+          <th style={attendContentStyle}>반이름</th>
+          <th style={attendContentStyle}>학생이름</th>
+          <th style={attendContentStyle}>출결상태</th>
         </tr>
       </thead>
       <tbody>
         {attendanceData.map((record, index) => (
-          <tr key={index}>
-            <td>{record.date.replace(/T.*$/, "")}</td>
-            <td>{record.className}</td>
-            <td>{record.studentName}</td>
-            <td>{record.status}</td>
+          <tr key={index} style={attendTableStyle}>
+            <td style={attendContentStyle}>
+              {record.date.replace(/T.*$/, "")}
+            </td>
+            <td style={attendContentStyle}>{record.className}</td>
+            <td style={attendContentStyle}>{record.studentName}</td>
+            <td style={attendContentStyle}>{record.status}</td>
           </tr>
         ))}
       </tbody>
--- src/components/organisms/AttendanceCheck.tsx
+++ src/components/organisms/AttendanceCheck.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import AttendanceTable from "../atom/AttendanceTable";
 
 const AttendanceCheck: React.FC = () => {
+  const attendancecheckStyle: CSSProperties = {
+    maxWidth: "1200px",
+    width: "100%",
+    margin: "0 auto",
+  };
   return (
-    <div>
-      <h1>출결 확인</h1>
+    <div style={attendancecheckStyle}>
+      <h1 style={{ width: "100%", textAlign: "center" }}>출결 확인</h1>
       <AttendanceTable />
     </div>
   );
